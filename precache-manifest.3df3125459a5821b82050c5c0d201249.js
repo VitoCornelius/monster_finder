2be52b389a3d5ca84c7cf6c519553c1b420d6855
Rafal ROjek <rafalrojek90@gmail.com>
2019-08-14 18:02:48 +0200

    Updates

---
+++ precache-manifest.3df3125459a5821b82050c5c0d201249.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49cbf0d04e4bde3739ff89ceb0067e14",
+    "url": "/monster_finder/index.html"
+  },
+  {
+    "revision": "417c4870fc68feec8555",
+    "url": "/monster_finder/static/css/main.d2f2da25.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/monster_finder/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "417c4870fc68feec8555",
+    "url": "/monster_finder/static/js/main.aec8ca57.chunk.js"
+  },
+  {
+    "revision": "693973cdc71212c16422",
+    "url": "/monster_finder/static/js/runtime~main.10c2efbd.js"
+  }
+]);
